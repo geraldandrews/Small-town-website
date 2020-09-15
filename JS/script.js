53be231d6f1be3b6beae1dc130132ca94453563c
Gerald Andrews <gerald2399@comcast.net>
2020-09-15 15:39:56 -0400

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.photo-items').magnificPopup({
+        type: 'image',
+        delegate: 'a',
+        gallery: {
+            enabled: true
+        }
+
+    });
+  
+});
+  
+  
+  
