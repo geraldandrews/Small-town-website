b7178ebb3a2c3015969282743c264b121cc0b847
Gerald Andrews <gerald2399@comcast.net>
2023-07-21 22:48:27 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,42 @@
-$(document).ready(function () {
-    $('.photo-items').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
+// NAVIGATION BAR //
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
+const hamburger = document.getElementById('hamburger');
+const dropDown = document.querySelector('.dropdown');
+const subMenu = document.querySelector('.sub-menu');
+const menuLinks = document.querySelector('.menu-links');
 
-    });
-  
+hamburger.addEventListener('click', function(e) {
+  hamburger.classList.toggle('cross');
 });
+
+navToggle.addEventListener("click", function () {
+   if (links.classList.contains("show-links")) {
+     links.classList.remove("show-links");
+     links.classList.remove("extend-navbar");
+   } else {
+     links.classList.add("show-links");
+   }
+}); 
+
+dropDown.addEventListener("click", function () {
+     if (subMenu.classList.contains("show-dropdown")) {
+       subMenu.classList.remove("show-dropdown");
+       links.classList.remove("extend-navbar");
+       menuLinks.classList.add("active");
+    } else {
+       subMenu.classList.add("show-dropdown");
+       links.classList.add("extend-navbar");
+       menuLinks.classList.remove("active");
+     }
+  });
+
+  document.addEventListener('click', function(event){
+    const yourContainer = document.querySelector('nav-links');
+    if(!links.contains(event.target)) {
+       links.classList.remove("extend-navbar");
+    }
+ });
   
   
   
