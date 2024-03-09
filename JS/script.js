b675e633564eb790b672d7e6c3757dec8aec6965
Gerald Andrews <gerald2399@comcast.net>
2024-03-09 17:00:56 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -47,17 +47,8 @@ let anchorList = document.querySelectorAll(anchorSelector);
       hamburger.classList.toggle('cross');
       subMenu.classList.remove("show-dropdown");
       links.classList.remove("extend-navbar");
-
-      e.preventDefault();
-
-        let destination = document.querySelector(this.hash);
-        destination.scrollIntoView({
-            behavior: 'smooth'
-        });
     }
-});
-
-    
+  });
  
 
 
