2db415752d778a259cc729f17913ab587549f991
Gerald Andrews <gerald2399@comcast.net>
2024-03-23 21:57:01 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -58,6 +58,25 @@ function closeMenu() {
     }
  });
 
+// Page Transition //
+document.addEventListener("DOMContentLoaded", () => {
+  const transitionElement = document.querySelector('.transition');
+  const anchors = document.querySelectorAll('#page'); 
+    for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500)
+    });
+  }
+});
+
+
 
 
  
